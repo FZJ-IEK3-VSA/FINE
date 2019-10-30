ab428583f9359bcac61588e45719a8d691901f72
Shruthi Patil <s.patil@fz-juelich.de>
2019-10-30 15:11:18 +0100

    test_aggregate_time_series is implemented

--- tests/test_representation.py
+++ tests/test_representation.py
@@ -1,7 +1,10 @@
 import pytest
+import numpy as np 
+import xarray as xr 
 
 import spagat.dataset as spd
 import spagat.representation as spr
+import spagat.grouping as spg
 
 
 @pytest.mark.skip()
@@ -27,12 +30,35 @@ def test_aggregate_geometries():
     pass
 
 
-@pytest.mark.skip()
+#@pytest.mark.skip()
 def test_aggregate_time_series():
-    pass
+     
+     #import dataset 
+     ds = xr.open_dataset('tests/data/input/sds_xr_dataset.nc4')
+
+     #get the dictonary output by string_based_clustering function
+     dict_ds = spg.string_based_clustering(ds['region_ids'].values)
+    
+     #A test_xr_DataArray is created with dummy values, coordinates and dimensions being region_ids and time
+     region_ids = ds['region_ids'].values
+     time = ds['time'].values
+
+     temp_num = 60*8760 
+     xr_DataArray_values = np.ones(temp_num)*5  #create array of 5s
+     xr_DataArray_values = np.reshape(xr_DataArray_values, (60,8760))
+
+     test_xr_DataArray = xr.DataArray(xr_DataArray_values, coords=[region_ids,time], dims=['region_ids','time'])
+     
+     #call the function 
+     ##modes have to be changed, xr_weight_array has to be provided in case of weighted mean  
+     mode = 'weighted mean'
+     xr_weight_array = test_xr_DataArray   #for weighted mean
+     time_series_aggregated = spr.aggregate_time_series(test_xr_DataArray, dict_ds, mode=mode, xr_weight_array=xr_weight_array)
+
+     #correct values for mean = 5, weighted mean = 5, sum = 25  for loc[4,'de']
+     assert time_series_aggregated.loc[4,'de'].values == 5   
 
 
-# spagat.output
 @pytest.mark.skip()
 def test_create_grid_shapefile():
     pass
