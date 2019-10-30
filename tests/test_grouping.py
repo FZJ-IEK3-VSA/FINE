dd90580e026e434980fb7f3e441b508c8020108f
Shruthi Patil <s.patil@fz-juelich.de>
2019-10-30 11:14:07 +0100

    implemented test

--- tests/test_grouping.py
+++ tests/test_grouping.py
@@ -1,8 +1,17 @@
 import pytest
+import xarray as xr 
+import geopandas
+import geoplot 
 
 import spagat.grouping as spg
+import spagat.dataset as spd
 
+#read e-highway shapefile 
+path_to_ehighwayfile = "/home/s-patil/data/e-highway"
+ehighway_shapefile = geopandas.read_file(path_to_ehighwayfile, layer='e-highway')
+#@pytest.mark.skip()
 
-@pytest.mark.skip()
 def test_string_based_clustering():
-    pass
+     clustered_regions = spg.string_based_clustering(ehighway_shapefile['e-id'])
+     assert len(clustered_regions.keys()) == 42
+      
