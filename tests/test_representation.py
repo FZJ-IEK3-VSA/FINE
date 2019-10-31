281fe6830c3ccd30b6f9b6bdf9f1f8bba5c473ec
Shruthi Patil <s.patil@fz-juelich.de>
2019-10-31 15:22:34 +0100

    implemented test_aggregate_based_on_sub_to_sup_region_id_dict

--- tests/test_representation.py
+++ tests/test_representation.py
@@ -6,6 +6,8 @@ import spagat.dataset as spd
 import spagat.representation as spr
 import spagat.grouping as spg
 
+import metis_utils.io_tools as ito
+
 
 @pytest.mark.skip()
 def test_add_region_centroids_and_distances():
@@ -18,11 +20,48 @@ def test_add_region_centroids_and_distances():
     spr.add_centroid_distances(sds)
 
 
-@pytest.mark.skip()
+#@pytest.mark.skip()
 def test_aggregate_based_on_sub_to_sup_region_id_dict():
-    sds = spd.SpagatDataSet()
+    
+     #read the xr_dataset and manipulate it to obtain a test xarray dataset
+     test_xr_dataset = xr.open_dataset('tests/data/input/sds_xr_dataset.nc4')
+     test_xr_dataset = test_xr_dataset.where(test_xr_dataset==1, other=1)
+     
+     #save the data in the same folder where the shapefile is 
+     test_xr_dataset.to_netcdf('tests/data/input/test_xr_dataset.nc4')  
 
-    # ...
+     #get the dictonary output by string_based_clustering function
+     test_xr_dataset_dict = spg.string_based_clustering(test_xr_dataset['region_ids'].values)
+     
+     #get the sds form of the data and shapefile 
+     test_sds = spd.SpagatDataSet()
+     test_sds.read_sds(sds_folder_path='tests/data/input' , sds_regions_filename='sds_regions.shp', 
+                       sds_xr_dataset_filename='test_xr_dataset.nc4')
+     
+     #run the function 
+     test_output_sds = spr.aggregate_based_on_sub_to_sup_region_id_dict(test_sds, test_xr_dataset_dict)
+     test_output_xr_dataset = test_output_sds.xr_dataset
+     
+     #assert functions 
+     ##testing aggregate_connections, with mode='bool'
+     assert test_output_sds.xr_dataset['AC_cable_incidence'].loc['de', 'es']== 1
+     ##testing aggregate_connections, with mode='sum'
+     assert test_output_sds.xr_dataset['AC_cable_capacity'].loc['de', 'es'] == 25
+     ##testing aggregate_time_series, with mode='sum'
+     assert test_output_xr_dataset['e_load'].loc['de', 8].values == 5
+     ##testing aggregate_time_series, with mode='weighted mean'
+     assert test_output_sds.xr_dataset['wind cf'].loc['de', 6] == 1
+     ##testing aggregate_values, with mode='sum'
+     assert test_output_sds.xr_dataset['wind capacity'].loc['de'] == 5
+
+     #assert test_output_sds.add_objects 
+
+     #assert add_region_centroids(test_output_sds)
+
+    
+
+    
+    
 
 
 @pytest.mark.skip()
