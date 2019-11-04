a0c55a19f979b9912aa80b034399f541de0c604c
Robin Beer <r.beer@fz-juelich.de>
2019-11-04 16:15:13 +0100

    minor changes

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -13,3 +13,7 @@ def test_read_and_save_sds():
 
     sds.read_dataset(sds_folder_path_in)
     sds.save_sds(sds_folder_path_out)
+
+def test_add_time_series_from_csv():
+    '''Region time series from a csv file are read and added to the dataset'''
+
