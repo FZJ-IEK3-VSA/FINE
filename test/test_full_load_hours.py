7d29bdb73d1778dfee535eab01a1c1d611bc9c8d
Robin Beer <r.beer@fz-juelich.de>
2019-12-04 21:53:53 +0100

    added skeleton for fixtures

--- test/test_full_load_hours.py
+++ test/test_full_load_hours.py
@@ -1,23 +1,14 @@
-import numpy as np
 
 
-# fixture for pyomo model
-# 
+def test_func(minimal_test_esM):
+    # modify full load hour limit
+    esM = minimal_test_esM
 
-@pytest.mark.parametrize('time_series, full_load_hour_limit, expected', 
-                         [
-                            # ((np.random(), 5000), True), 
-                            (np.random(8760), 8760, True)
-                            (np.random(8760), 1, False)
-                            # ((np.random()), True), 
-                            # time series with different resolutions and # time steps
-                            # capacity factor time series VS. operation time series
 
-                         ])
-def test_full_load_hour_below_limit():
+    assert optimization_without_full_load_hour == False
 
-    assert full_load_hour_below_limit(time_series, capacity) == expected 
-    
+    # add full load hour constraint
+    assert optimization_without_full_load_hour == True
 
 
 
