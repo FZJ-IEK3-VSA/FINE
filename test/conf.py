f9a84c0b1ce098b62a6186dc0ca053c3aadd1824
l-kotzur <l.kotzur@fz-juelich.de>
2019-12-05 10:45:25 +0100

    include fullloadhour tests

--- test/conf.py
+++
@@ -1,4 +0,0 @@
-
-
-def minimal_test_esM():
-    """Returns minimal instance of esM"""
--- test/test_full_load_hours.py
+++ test/test_full_load_hours.py
@@ -1,14 +1,78 @@
 
 
-def test_func(minimal_test_esM):
-    # modify full load hour limit
-    esM = minimal_test_esM
+def test_fullloadhours_above(minimal_test_esM):
+   '''
+   Get the minimal test system, and check if the fulllload hours of electrolyzer are above 4000.
+   '''
+   esM = minimal_test_esM
+
+   esM.optimize(timeSeriesAggregation=False, solver = 'glpk')
+
+   # get cumulative operation   
+   operationSum = esM.componentModelingDict["ConversionModel"].operationVariablesOptimum.xs('Electrolyzers').sum().sum()
+
+   # get capacity
+   capacitySum = esM.componentModelingDict["ConversionModel"].capacityVariablesOptimum.xs('Electrolyzers').sum()
+
+   # calculate fullloadhours
+   fullloadhours = operationSum/capacitySum
+   
+   assert fullloadhours > 4000.
 
 
-    assert optimization_without_full_load_hour == False
+def test_fullloadhours_max(minimal_test_esM):
+   '''
+   Get the minimal test system, and check if the fulllload hour limitation works
+   '''
+   
+   # modify full load hour limit
+   esM = minimal_test_esM
 
-    # add full load hour constraint
-    assert optimization_without_full_load_hour == True
+   # get the electolyzer 
+   electrolyzer = esM.getComponent('Electrolyzers')
+
+   # set fullloadhour limit
+   electrolyzer.yearlyFullLoadHourMax = 3000.
+
+   # optimize
+   esM.optimize(timeSeriesAggregation=False, solver = 'glpk')
+
+   # get cumulative operation   
+   operationSum = esM.componentModelingDict["ConversionModel"].operationVariablesOptimum.xs('Electrolyzers').sum().sum()
+
+   # get capacity
+   capacitySum = esM.componentModelingDict["ConversionModel"].capacityVariablesOptimum.xs('Electrolyzers').sum()
+
+   # calculate fullloadhours
+   fullloadhours = operationSum/capacitySum
+   
+   assert fullloadhours < 3000.01
 
 
+def test_fullloadhours_min(minimal_test_esM):
+   '''
+   Get the minimal test system, and check if the fulllload hour limitation works
+   '''
+   
+   # modify full load hour limit
+   esM = minimal_test_esM
 
+   # get the electolyzer 
+   electrolyzer = esM.getComponent('Electrolyzers')
+
+   # set fullloadhour limit
+   electrolyzer.yearlyFullLoadHourMin = 5000.
+
+   # optimize
+   esM.optimize(timeSeriesAggregation=False, solver = 'glpk')
+
+   # get cumulative operation   
+   operationSum = esM.componentModelingDict["ConversionModel"].operationVariablesOptimum.xs('Electrolyzers').sum().sum()
+
+   # get capacity
+   capacitySum = esM.componentModelingDict["ConversionModel"].capacityVariablesOptimum.xs('Electrolyzers').sum()
+
+   # calculate fullloadhours
+   fullloadhours = operationSum/capacitySum
+   
+   assert fullloadhours > 4999.99
