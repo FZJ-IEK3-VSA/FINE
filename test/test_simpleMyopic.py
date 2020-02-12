762d3ebc8a0821bc5817414b62274da50986eb77
t-gross <t.gross@fz-juelich.de>
2020-02-12 10:13:51 +0100

    Adapt test for simple myopic

--- test/test_simpleMyopic.py
+++ test/test_simpleMyopic.py
@@ -4,12 +4,61 @@ import pandas as pd
 
 np.random.seed(42)        # Sets a "seed" to produce the same random input data in each model run
 
-def test_exceededLifetime(minimal_test_esM):
+def test_exceededLifetime():
     # load a minimal test system
-    esM= minimal_test_esM
+    """Returns minimal instance of esM"""
+
+    numberOfTimeSteps = 4
+    hoursPerTimeStep = 2190
+
+    # Create an energy system model instance 
+    esM = fn.EnergySystemModel(locations={'OneLocation'}, 
+                                commodities={'electricity', 'hydrogen'}, 
+                                numberOfTimeSteps=numberOfTimeSteps,
+                                commodityUnitsDict={'electricity': r'kW$_{el}$', 'hydrogen': r'kW$_{H_{2},LHV}$'},
+                                hoursPerTimeStep=hoursPerTimeStep, costUnit='1 Euro', 
+                                lengthUnit='km', 
+                                verboseLogLevel=2)
+
+    ### Buy electricity at the electricity market
+    costs = pd.DataFrame([np.array([ 0.05, 0., 0.1, 0.051,])],
+                            index = ['OneLocation']).T
+    revenues = pd.DataFrame([np.array([ 0., 0.01, 0., 0.,])],
+                            index = ['OneLocation']).T
+    maxpurchase = pd.DataFrame([np.array([1e6, 1e6, 1e6, 1e6,])],
+                            index = ['OneLocation']).T * hoursPerTimeStep
+    esM.add(fn.Source(esM=esM, name='Electricity market', commodity='electricity', 
+                        hasCapacityVariable=False, operationRateMax = maxpurchase,
+                        commodityCostTimeSeries = costs,  
+                        commodityRevenueTimeSeries = revenues,  
+                        )) # eur/kWh
+
+    ### Electrolyzers
+    esM.add(fn.Conversion(esM=esM, name='Electrolyzers', physicalUnit=r'kW$_{el}$',
+                          commodityConversionFactors={'electricity':-1, 'hydrogen':0.7},
+                          hasCapacityVariable=True, 
+                          investPerCapacity=500, # euro/kW
+                          opexPerCapacity=500*0.025, 
+                          interestRate=0.08,
+                          economicLifetime=10))
+
+    ### Hydrogen filled somewhere
+    esM.add(fn.Storage(esM=esM, name='Pressure tank', commodity='hydrogen',
+                       hasCapacityVariable=True, capacityVariableDomain='continuous',
+                       stateOfChargeMin=0.33, 
+                       investPerCapacity=0.5, # eur/kWh
+                       interestRate=0.08,
+                       economicLifetime=30))
+
+    ### Industry site
+    demand = pd.DataFrame([np.array([6e3, 6e3, 6e3, 6e3,])],
+                    index = ['OneLocation']).T * hoursPerTimeStep
+    esM.add(fn.Sink(esM=esM, name='Industry site', commodity='hydrogen', hasCapacityVariable=False,
+                    operationRateFix = demand,
+                    ))
 
     # Set the technical lifetime of the electrolyzers to 7 years.
-    setattr(esM.componentModelingDict['ConversionModel'].componentsDict['Electrolyzers'], 'technicalLifetime', pd.Series([7,7], index=esM.locations))
+    setattr(esM.componentModelingDict['ConversionModel'].componentsDict['Electrolyzers'], 'technicalLifetime', pd.Series([7], index=['OneLocation']))
 
     results = fn.optimizeSimpleMyopic(esM, startYear=2020, endYear=2030, nbOfRepresentedYears=5, timeSeriesAggregation=False, solver='glpk', saveResults=False)
 
