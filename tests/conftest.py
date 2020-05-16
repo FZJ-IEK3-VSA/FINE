a03ba7ac84173c6e54ba82556e90a3291a482a76
Robin Beer <r.beer@fz-juelich.de>
2020-05-16 12:17:36 +0200

    changed paths to spagat/tests + minor changes including env.yml updates

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,9 +5,9 @@ import pathlib
 import pytest
 
 
-@pytest.fixture(scope='package')
+@pytest.fixture(scope="package")
 def sds():
-    sds_folder_path_in = pathlib.Path('tests/data/input')
+    sds_folder_path_in = pathlib.Path("spagat/tests/data/input")
     sds = spd.SpagatDataSet()
     sds.read_dataset(sds_folder_path_in)
     spr.add_region_centroids(sds)
