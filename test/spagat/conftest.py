5627821796e3209f29c54e566da9452b2ed2ddf4
s.patil <s.patil@fz-juelich.de>
2020-11-24 16:53:37 +0100

    Fixtures added to test representation functions

--- test/spagat/conftest.py
+++ test/spagat/conftest.py
@@ -2,6 +2,7 @@ import pathlib
 import numpy as np
 import pytest
 import xarray as xr 
+from shapely.geometry import Polygon
 
 import FINE.spagat.dataset as spd
 import FINE.spagat.representation as spr
@@ -78,7 +79,90 @@ def test_dataset2():   #TODO: rename this fixture to something more descriptive
     return sds
 
 
+@pytest.fixture()
+def sds_for_basic_representation():  
+  '''
+  sds data to test basic representation functions-
+  1. test_aggregate_based_on_sub_to_sup_region_id_dict()
+  2. test_aggregate_time_series()
+  3. test_aggregate_values()
+  4. test_aggregate_connections()
+  5. test_create_grid_shapefile()
+  '''
+
+  component_list = ['c1','c2']  
+  space_list = ['01_reg','02_reg','03_reg','04_reg']
+  TimeStep_list = ['T0','T1']
+  Period_list = [0]
+
+  ## ts variable data
+  test_ts_data = np.array([ [ [ [5, 5, 5, 5] for i in range(2)] ],
+
+                          [ [[np.nan, np.nan, np.nan, np.nan] for i in range(2)] ]
+
+                          ])
+
+  test_ts_DataArray = xr.DataArray(test_ts_data, 
+                                  coords=[component_list, Period_list, TimeStep_list, space_list], 
+                                  dims=['component', 'Period', 'TimeStep','space'])
+
+  ## 1d variable data
+  test_1d_data = np.array([ [5,  5,  5, 5],
+                            [np.nan] *4
+                          ])
+
+  test_1d_DataArray = xr.DataArray(test_1d_data, 
+                              coords=[component_list, space_list], 
+                              dims=['component', 'space'])
+
+  ## 2d variable data
+  test_2d_data = np.array([ [[ 0,  5,  5, 5],
+                          [ 5,  0,  5, 5],
+                          [ 5,  5,  0, 5],
+                          [ 5,  5,  5, 0]],
+
+                        [[np.nan] * 4 for i in range(4)] 
+                        ])
+
+  test_2d_DataArray = xr.DataArray(test_2d_data, 
+                              coords=[component_list, space_list, space_list], 
+                              dims=['component', 'space', 'space_2'])
+
+  test_ds = xr.Dataset({'var_ts': test_ts_DataArray, 
+                        'var_1d': test_1d_DataArray,
+                        'var_2d': test_2d_DataArray})    
+
+  sds = spd.SpagatDataset()
+  sds.xr_dataset = test_ds            
+
+  test_geometries = [Polygon([(0,0), (2,0), (2,2), (0,2)]),
+                    Polygon([(2,0), (4,0), (4,2), (2,2)]),
+                    Polygon([(0,0), (4,0), (4,4), (0,4)]),
+                    Polygon([(0,0), (1,0), (1,1), (0,1)])]   
+
+  sds.add_objects(description ='gpd_geometries',   #NOTE: not sure if it is ok to call another function here
+                dimension_list =['space'], 
+                object_list = test_geometries)   
+
+  return sds      
+
+@pytest.fixture()
+def sub_to_sup_region_id_dict(): 
+  '''
+  sub_to_sup_region_id_dict to test basic representation functions-
+  1. test_aggregate_based_on_sub_to_sup_region_id_dict()
+  2. test_aggregate_time_series()
+  3. test_aggregate_values()
+  4. test_aggregate_connections()
+  5. test_aggregate_geometries()
+  '''
+  sub_to_sup_region_id_dict = {'01_reg_02_reg': ['01_reg','02_reg'], 
+                                 '03_reg_04_reg': ['03_reg','04_reg']}
   
+  return sub_to_sup_region_id_dict
+
+
+
 
 
 
