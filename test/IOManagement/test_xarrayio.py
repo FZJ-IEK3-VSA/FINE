cc41a8535d45abc160b656e51f78e8f98824aec5
s.patil <s.patil@fz-juelich.de>
2020-12-09 16:50:15 +0100

    test_update_dicts_based_on_xarray_dataset() implemented

--- test/IOManagement/test_xarrayio.py
+++ test/IOManagement/test_xarrayio.py
@@ -7,9 +7,16 @@ import geopandas as gpd
 
 import FINE as fn
 
+from FINE.IOManagement import dictIO
+import FINE.IOManagement.xarray_io as xrio 
+
+import FINE.spagat.dataset as spd
+import FINE.spagat.grouping as spg
+import FINE.spagat.representation as spr 
+
 def test_generate_iteration_dicts(minimal_test_esM):
-    esm_dict, component_dict = fn.dictIO.exportToDict(minimal_test_esM)
-    output_df_dict, output_series_dict = fn.xarray_io.generate_iteration_dicts(esm_dict, component_dict)
+    esm_dict, component_dict = dictIO.exportToDict(minimal_test_esM)
+    output_df_dict, output_series_dict = xrio.generate_iteration_dicts(esm_dict, component_dict)
 
     assert output_series_dict.get('locationalEligibility') == [('Source', 'Electricity market'), 
                                                                 ('Sink', 'Industry site'), 
@@ -28,7 +35,7 @@ def test_generate_iteration_dicts(minimal_test_esM):
                             'operationRateFix': [('Sink', 'Industry site')]} 
 
 def test_dimensional_data_to_xarray_dataset(minimal_test_esM):
-    esm_dict, component_dict = fn.dictIO.exportToDict(minimal_test_esM)
+    esm_dict, component_dict = dictIO.exportToDict(minimal_test_esM)
 
     expected_locations = list(minimal_test_esM.locations).sort()
     expected_Electricitymarket_operationRateMax = \
@@ -37,7 +44,7 @@ def test_dimensional_data_to_xarray_dataset(minimal_test_esM):
         minimal_test_esM.getComponentAttribute('Electrolyzers', 'investPerCapacity').values
     
     #FUNCTION CALL
-    output_xarray = fn.xarray_io.dimensional_data_to_xarray_dataset(esm_dict, component_dict)
+    output_xarray = xrio.dimensional_data_to_xarray_dataset(esm_dict, component_dict)
     ## a time series variable
     output_locations = list(output_xarray.space.values).sort()
     output_Electricitymarket_operationRateMax = \
@@ -58,10 +65,35 @@ def test_dimensional_data_to_xarray_dataset(minimal_test_esM):
                       (output_Pipelines_investPerCapacity[0][1] == 0.0885)
 
     
+def test_update_dicts_based_on_xarray_dataset(multi_node_test_esM_init):
+    # TEST DATA
+    #NOTE: aggregated_xr_dataset is obtained by running spatial_aggregation(),
+    # distance_based clustering with all default settings.
+    # Representation is performed for nRegionsForRepresentation = 2 
+    TEST_FILE_PATH = os.path.join(os.path.dirname(__file__), '../../test/spagat/data/input/aggregated_sds_xr_dataset.nc4')
+    aggregated_xr_dataset = xr.open_dataset(TEST_FILE_PATH)
 
-@pytest.mark.skip('To be implemented only if the function is useful')
-def test_update_dicts_based_on_xarray_dataset():
-    pass 
+    esm_dict, comp_dict = dictIO.exportToDict(multi_node_test_esM_init)
+    
+    #EXPECTED 
+    expected_locations = list(aggregated_xr_dataset.space.values).sort()
+    expected_opexPerCapacity_Windoffshore = \
+        sorted(aggregated_xr_dataset['1d_opexPerCapacity'].loc['Source, Wind (offshore)', :].values)
+
+    #FUNCTION CALL 
+    output_esm_dict, output_comp_dict = \
+        xrio.update_dicts_based_on_xarray_dataset(esm_dict, 
+                                                comp_dict, 
+                                                xarray_dataset=aggregated_xr_dataset)
+    
+    output_locations = list(output_esm_dict.get('locations')).sort()
+    output_opexPerCapacity_Windoffshore = \
+        sorted(output_comp_dict.get('Source').get('Wind (offshore)').get('opexPerCapacity').values, key=float)
+    
+    #ASSERTION 
+    assert output_locations == expected_locations
+    assert output_opexPerCapacity_Windoffshore == expected_opexPerCapacity_Windoffshore
+     
 
 @pytest.mark.skip('Needs to be adapted to changes in the corresponding function')
 def test_spatial_aggregation_multinode(multi_node_test_esM_init, solver):   #TODO: after fixing the spatial_aggregation function, rewrite this test WITH ASSERT STATEMENT 
