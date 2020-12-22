2d026da5d9dc5fa57b64aa3d4aa069dec682a33c
s.patil <s.patil@fz-juelich.de>
2020-12-22 15:25:58 +0100

    Solver changed to glpk

--- test/test_warnings.py
+++ test/test_warnings.py
@@ -5,14 +5,12 @@ from FINE import utils
 
 def test_userWarnings_esm(minimal_test_esM):
     '''
-    Tests if the warnings are supressed only when intended and shown otherwise in energySystemModel.py!
+    Tests if the warnings are supressed only when intended and shown otherwise in energySystemModel.py
     '''
-    
-    #TESTING FOR energySystemModel.py
     with warnings.catch_warnings(record=True) as w:
         timeSeriesAggregation=False
-        solver='gurobi'
-        # Code
+        solver='glpk'
+
         minimal_test_esM.optimize(timeSeriesAggregation=timeSeriesAggregation, solver=solver)
         minimal_test_esM.getOptimizationSummary('SourceSinkModel', outputLevel=5)
         
@@ -22,11 +20,10 @@ def test_userWarnings_esm(minimal_test_esM):
     
 def test_userWarnings_utils(minimal_test_esM):
     '''
-    Tests if the warnings are shown in utils.
+    Tests if the warnings are shown in utils.py
     '''
-    #TESTING FOR utils.py
     with warnings.catch_warnings(record=True) as w:
-        # Code
         utils.checkSinkCompCO2toEnvironment(minimal_test_esM, CO2ReductionTargets=1)
+        
         assert 'CO2 emissions are not considered in the current esM. CO2ReductionTargets will be ignored.' in str(w[-1].message)
 
