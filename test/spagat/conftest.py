8a6bd534b85c74451e7403d41c6814df5a60ff8a
s.patil <s.patil@fz-juelich.de>
2021-01-05 14:48:02 +0100

    Deleted commented out fixtures

--- test/spagat/conftest.py
+++ test/spagat/conftest.py
@@ -18,69 +18,6 @@ def sds():
 
     return sds
 
-# #TODO: If test_dataset1 and 2 are not used anywhere, remove them
-# @pytest.fixture()
-# def test_dataset1():   
-#     """
-#     Create a simple Test Xarray Dataset containing three variables (without component): opFix(time series var), 1d_cap, 2d_dist
-#     """
-#     space = ['01_reg','02_reg','03_reg']
-#     timestep = ['T0','T1']
-#     space_2 = space.copy()
-
-#     opFix = xr.DataArray(np.array([[1,1],
-#                                     [0.9,1],
-#                                     [2,2]]), coords=[space, timestep], dims=['space', 'TimeStep'])
-#     cap_1d = xr.DataArray(np.array([0.9,
-#                                        1,
-#                                        0.9]), coords=[space], dims=['space'])
-#     dist_2d = xr.DataArray(np.array([[0,1,2],
-#                                       [1,0,10],
-#                                       [2,10,0]]), coords=[space,space_2], dims=['space','space_2'])
-
-#     ds = xr.Dataset({'operationFixRate': opFix,'1d_capacity': cap_1d,'2d_distance': dist_2d})
-
-#     sds = spd.SpagatDataset()
-#     sds.xr_dataset = ds
-#     return sds
-
-
-# @pytest.fixture()
-# def test_dataset2():   
-#     """
-#     Create a Test Xarray Dataset: each variable has several components
-#     """
-#     space = ['01_reg','02_reg','03_reg']
-#     TimeStep = ['T0','T1']
-#     space_2 = space.copy()
-#     component = ['c1','c2','c3','c4']
-#     Period = [0]
-
-#     demand = np.stack([[[[np.nan,np.nan, np.nan] for i in range(2)]],
-#                         [[[1, 0.9,  2],
-#                           [1, 0,  0.9]]],
-#                         [[[np.nan,np.nan, np.nan] for i in range(2)]],
-#                         [[[0,   1, 1],
-#                           [0.3, 2, 1]]]])
-#     demand = xr.DataArray(demand, coords=[component, Period, TimeStep, space], dims=['component', 'Period', 'TimeStep','space'])
-#     cap_1d = np.stack([[0.9,  1,  0.9],
-#                         [0,    0,  0],
-#                         [0.9,  1,  0.9],
-#                         [np.nan] *3])
-#     cap_1d = xr.DataArray(cap_1d, coords=[component,space], dims=['component','space'])
-#     dist_2d = np.stack([[[0,1,2],[1,0,10],[2,10,0]],
-#                          [[0,0.1,0.2],[0.1,0,1],[0.2,1,0]],
-#                          [[np.nan] * 3 for i in range(3)],
-#                          [[np.nan] * 3 for i in range(3)]])
-#     dist_2d = xr.DataArray(dist_2d, coords=[component,space,space_2], dims=['component','space','space_2'])
-
-#     ds = xr.Dataset({'operationFixRate': demand, '1d_capacity': cap_1d, '2d_distance': dist_2d})
-
-#     sds = spd.SpagatDataset()
-#     sds.xr_dataset = ds
-#     return sds
-
-
 @pytest.fixture()
 def sds_and_dict_for_basic_representation():  
   '''
