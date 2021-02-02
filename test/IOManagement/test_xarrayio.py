48cc942f587d033eecd3a062c22572686d25d948
s.patil <s.patil@fz-juelich.de>
2021-02-02 10:59:33 +0100

    tests for spatial aggeregation moved

--- test/IOManagement/test_xarrayio.py
+++ test/IOManagement/test_xarrayio.py
@@ -10,9 +10,6 @@ import FINE as fn
 from FINE.IOManagement import dictIO
 import FINE.IOManagement.xarray_io as xrio 
 
-import FINE.spagat.dataset as spd
-import FINE.spagat.grouping as spg
-import FINE.spagat.representation as spr 
 
 def test_generate_iteration_dicts(minimal_test_esM):
     esm_dict, component_dict = dictIO.exportToDict(minimal_test_esM)
@@ -94,63 +91,7 @@ def test_update_dicts_based_on_xarray_dataset(multi_node_test_esM_init):
     assert output_locations == expected_locations
     assert output_opexPerCapacity_Windoffshore == expected_opexPerCapacity_Windoffshore
      
-def test_spatial_aggregation_string_based(multi_node_test_esM_init, 
-                                          solver):   
-    
-    SHAPEFILE_PATH = os.path.join(os.path.dirname(__file__), \
-        '../../examples/Multi-regional_Energy_System_Workflow/', 
-            'InputData/SpatialData/ShapeFiles/clusteredRegions.shp')
-    regions_gdf = gpd.read_file(SHAPEFILE_PATH)
 
-    #FUNCTION CALL 
-    aggregated_esM = xrio.spatial_aggregation(esM = multi_node_test_esM_init, 
-                                            gdfRegions = regions_gdf, 
-                                            grouping_mode = 'string_based', 
-                                            aggregation_function_dict = None,                             
-                                            aggregatedResultsPath=None,                        
-                                            agg_mode='sklearn_spectral2')   
-
-    #ASSERTION 
-    assert len(aggregated_esM.locations) == 8
-    # Additional check - if the optimization runs through
-    aggregated_esM.cluster(numberOfTypicalPeriods=2)
-    aggregated_esM.optimize(timeSeriesAggregation=True, solver=solver)
-
-    
-@pytest.mark.parametrize("grouping_mode, agg_mode", [('distance_based', 'sklearn_kmeans'), 
-                                                       ('distance_based', 'sklearn_spectral'),
-                                                       ('distance_based', 'scipy_hierarchical'),
-                                                       ('all_variable_based', 'scipy_hierarchical'),
-                                                       ('all_variable_based', 'spectral_with_precomputedAffinity'),
-                                                       ('all_variable_based', 'spectral_with_RBFaffinity')
-                                                    ])
-@pytest.mark.parametrize("n_regions", [1, 3, 8]) 
-def test_spatial_aggregation(multi_node_test_esM_init, 
-                            solver, 
-                            grouping_mode, 
-                            agg_mode,
-                            n_regions):   
-    
-    SHAPEFILE_PATH = os.path.join(os.path.dirname(__file__), \
-        '../../examples/Multi-regional_Energy_System_Workflow/', 
-            'InputData/SpatialData/ShapeFiles/clusteredRegions.shp')
-    regions_gdf = gpd.read_file(SHAPEFILE_PATH)
-
-    #FUNCTION CALL 
-    aggregated_esM = xrio.spatial_aggregation(esM = multi_node_test_esM_init, 
-                                            gdfRegions = regions_gdf, 
-                                            grouping_mode = grouping_mode, 
-                                            nRegionsForRepresentation = n_regions, 
-                                            aggregation_function_dict = None, 
-                                            aggregatedResultsPath=None,
-                                            agg_mode=agg_mode)   
-
-    #ASSERTION 
-    assert len(aggregated_esM.locations) == n_regions
-    # Additional check - if the optimization runs through
-    aggregated_esM.cluster(numberOfTypicalPeriods=2)
-    aggregated_esM.optimize(timeSeriesAggregation=True, solver=solver)
-    #TODO: add test to check if optimization summary is available
 
 
 
