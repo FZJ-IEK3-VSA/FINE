b7e3327b38dcb8147844c0c44d1c32fbd1319605
s.patil <s.patil@fz-juelich.de>
2021-05-19 10:33:16 +0200

    refactoring representation code

--- FINE/spagat/RE_representation.py
+++ FINE/spagat/RE_representation.py
@@ -44,7 +44,7 @@ def represent_RE_technology(gridded_RE_ds,
         - If set to 1, performs simple aggregation
             - Within every region, calculates the weighted mean of RE 
               time series (capacities being weights), and sums the capacities.
-        - If set to a value more than 1, time series clustering is employed
+        - If set to a value greateer than 1, time series clustering is employed
             - Clustering method: agglomerative hierarchical clustering
             - Distance measure: Euclidean distance
             - Aggregation within each resulting cluster is the same as simple 
@@ -66,7 +66,7 @@ def represent_RE_technology(gridded_RE_ds,
     geometry_col : str, optional (default='geometry')
         The column in `shp_file` that holds geometries 
     linkage : str, optional (default='average') 
-        - Relevant only if `n_timeSeries_perRegion` is more than 1. 
+        - Relevant only if `n_timeSeries_perRegion` is greater than 1. 
         - The linkage criterion to be used with agglomerative hierarchical clustering. 
           Can be 'complete', 'single', etc. Refer to Sklearn's documentation for more info.
 
@@ -75,45 +75,32 @@ def represent_RE_technology(gridded_RE_ds,
     represented_RE_ds : xr.Dataset 
     - Dimensions in this data - `time`, 'region_ids'
         - The dimension 'region_ids' has its coordinates corresponding to `index_col` 
-    If `n_timeSeries_perRegion` is more than 1, additional dimension - 'TS_ids' is present
+    If `n_timeSeries_perRegion` is greater than 1, additional dimension - 'TS_ids' is present
         -  Within each region, different time seires are indicated by this 'TS_ids'   
     """
 
-    def _simply_aggregate_RE_technology(region):
-    
-        #STEP 1. Create resultant xarray dataset 
-        time_steps = rasterized_RE_ds[time].values  
-        n_timeSteps = len(time_steps)
-
-        ## time series 
-        data = np.zeros((n_timeSteps, 1))
-
-        represented_timeSeries = xr.DataArray(data, [(time, time_steps),
-                                                    ('region_ids', [region])])
-
-        #capacities
-        represented_capacities = xr.DataArray(0, [('region_ids', [region])])
-
-        #STEP 2. Representation
+    def _preprocess_regional_xr_ds(region):
+        '''
+        Private function to preprocess regional data. 
+        '''
+        #Get regional data
         regional_ds = rasterized_RE_ds.sel(region_ids = region)
     
         regional_capfac_da = regional_ds[capfac_var_name].where(regional_ds.rasters == 1)
         regional_capacity_da = regional_ds[capacity_var_name].where(regional_ds.rasters == 1)
 
-        #STEP 2b. Preprocess regional capfac and capacity dataArrays 
-
-        #STEP 2b (i). Restructure data
+        #Restructure data
         regional_capfac_da = regional_capfac_da.stack(x_y = [longitude, latitude]) 
         regional_capfac_da = regional_capfac_da.transpose(transpose_coords= True) 
 
         regional_capacity_da = regional_capacity_da.stack(x_y = [longitude, latitude])
         regional_capacity_da = regional_capacity_da.transpose(transpose_coords= True)
 
-        #STEP 2b (ii). Remove all time series with 0 values 
+        #Remove all time series with 0 values 
         regional_capfac_da = regional_capfac_da.where(regional_capacity_da > 0)
         regional_capacity_da = regional_capacity_da.where(regional_capacity_da > 0)
 
-        #STEP 2b (iii). Drop NAs 
+        #Drop NAs 
         regional_capfac_da = regional_capfac_da.dropna(dim='x_y')
         regional_capacity_da = regional_capacity_da.dropna(dim='x_y')
 
@@ -121,105 +108,10 @@ def represent_RE_technology(gridded_RE_ds,
         n_ts = len(regional_capfac_da['x_y'].values)
         print(f'Number of time series in {region}: {n_ts}')
 
-        #STEP 2c. Get power curves from capacity factor time series and capacities 
+        #Get power curves from capacity factor time series and capacities 
         regional_power_da = regional_capacity_da * regional_capfac_da
 
-        #STEP 2d. Aggregation
-        ## capacity
-        capacity_total = regional_capacity_da.sum(dim = 'x_y').values
-        represented_capacities.loc[region] = capacity_total
-        
-        ## capacity factor 
-        power_total = regional_power_da.sum(dim = 'x_y').values
-        capfac_total = power_total/capacity_total
-        
-        represented_timeSeries.loc[:,region] = capfac_total
-
-        #STEP 3. Create resulting dataset 
-        regional_represented_RE_ds = xr.Dataset({capacity_var_name: represented_capacities,
-                                        capfac_var_name: represented_timeSeries}) 
-
-    
-        return regional_represented_RE_ds 
-
-
-    def _cluster_RE_technology(region):
-    
-        #STEP 1. Create resultant xarray dataset 
-        time_steps = rasterized_RE_ds[time].values  
-        n_timeSteps = len(time_steps)
-
-        TS_ids = [f'TS_{i}' for i in range(n_timeSeries_perRegion)] 
-
-        ## time series 
-        data = np.zeros((n_timeSteps, 1, n_timeSeries_perRegion))
-
-        represented_timeSeries = xr.DataArray(data, [(time, time_steps),
-                                                    ('region_ids', [region]),
-                                                    ('TS_ids', TS_ids)])
-
-        data = np.zeros((1, n_timeSeries_perRegion))
-
-        #capacities
-        represented_capacities = xr.DataArray(data, [('region_ids', [region]),
-                                                    ('TS_ids', TS_ids)])
-
-        #STEP 2. Representation
-        regional_ds = rasterized_RE_ds.sel(region_ids = region)
-    
-        regional_capfac_da = regional_ds[capfac_var_name].where(regional_ds.rasters == 1)
-        regional_capacity_da = regional_ds[capacity_var_name].where(regional_ds.rasters == 1)
-
-        #STEP 2b. Preprocess regional capfac and capacity dataArrays 
-
-        #STEP 2b (i). Restructure data
-        regional_capfac_da = regional_capfac_da.stack(x_y = [longitude, latitude]) 
-        regional_capfac_da = regional_capfac_da.transpose(transpose_coords= True) 
-
-        regional_capacity_da = regional_capacity_da.stack(x_y = [longitude, latitude])
-        regional_capacity_da = regional_capacity_da.transpose(transpose_coords= True)
-
-        #STEP 2b (ii). Remove all time series with 0 values 
-        regional_capfac_da = regional_capfac_da.where(regional_capacity_da > 0)
-        regional_capacity_da = regional_capacity_da.where(regional_capacity_da > 0)
-
-        #STEP 2b (iii). Drop NAs 
-        regional_capfac_da = regional_capfac_da.dropna(dim='x_y')
-        regional_capacity_da = regional_capacity_da.dropna(dim='x_y')
-
-        #Print out number of time series in the region 
-        n_ts = len(regional_capfac_da['x_y'].values)
-        print(f'Number of time series in {region}: {n_ts}')
-
-        #STEP 2c. Get power curves from capacity factor time series and capacities 
-        regional_power_da = regional_capacity_da * regional_capfac_da
-
-        #STEP 2d. Clustering  
-        agg_cluster = AgglomerativeClustering(n_clusters=n_timeSeries_perRegion, 
-                                              affinity="euclidean",  
-                                              linkage=linkage)
-        agglomerative_model = agg_cluster.fit(regional_capfac_da)
-
-        #STEP 2e. Aggregation
-        for i in range(np.unique(agglomerative_model.labels_).shape[0]):
-            ## Aggregate capacities 
-            cluster_capacity = regional_capacity_da[agglomerative_model.labels_ == i]
-            cluster_capacity_total = cluster_capacity.sum(dim = 'x_y').values
-
-            represented_capacities.loc[region, TS_ids[i]] = cluster_capacity_total
-
-            #aggregate capacity factor 
-            cluster_power = regional_power_da[agglomerative_model.labels_ == i]
-            cluster_power_total = cluster_power.sum(dim = 'x_y').values
-            cluster_capfac_total = cluster_power_total/cluster_capacity_total
-
-            represented_timeSeries.loc[:,region, TS_ids[i]] = cluster_capfac_total
-            
-        #STEP 3. Create resulting dataset 
-        regional_represented_RE_ds = xr.Dataset({capacity_var_name: represented_capacities,
-                                        capfac_var_name: represented_timeSeries})  
-
-        return regional_represented_RE_ds 
+        return regional_capfac_da, regional_capacity_da, regional_power_da
 
 
     #STEP 1. Rasterize the gridded dataset
@@ -231,17 +123,95 @@ def represent_RE_technology(gridded_RE_ds,
                                                     longitude, 
                                                     latitude)
 
-
     region_ids = rasterized_RE_ds['region_ids'].values 
+    n_regions = len(region_ids)
 
-    results =[]
+    time_steps = rasterized_RE_ds[time].values  
+    n_timeSteps = len(time_steps)
+    
+
     if n_timeSeries_perRegion==1:
+
+        #STEP 2. Create resultant xarray dataset 
+        ## time series 
+        data = np.zeros((n_timeSteps, n_regions))
+
+        represented_timeSeries = xr.DataArray(data, [(time, time_steps),
+                                                    ('region_ids', region_ids)])
+
+        #capacities
+        data = np.zeros(n_regions)
+        represented_capacities = xr.DataArray(data, [('region_ids', region_ids)])
+
+        #STEP 3. Aggregation in every region...
         for region in region_ids:
-            results.append(_simply_aggregate_RE_technology(region))
+            #Preprocess regional data    
+            regional_capfac_da, regional_capacity_da, regional_power_da = _preprocess_regional_xr_ds(region)
+            
+            #Aggregation
+            ## capacity
+            capacity_total = regional_capacity_da.sum(dim = 'x_y').values
+            represented_capacities.loc[region] = capacity_total
+            
+            ## capacity factor 
+            power_total = regional_power_da.sum(dim = 'x_y').values
+            capfac_total = power_total/capacity_total
+            
+            represented_timeSeries.loc[:,region] = capfac_total
+
+        #STEP 4. Create resulting dataset 
+        regional_represented_RE_ds = xr.Dataset({capacity_var_name: represented_capacities,
+                                                capfac_var_name: represented_timeSeries}) 
+
+        return regional_represented_RE_ds 
+
     else:
+
+        #STEP 2. Create resultant xarray dataset 
+        TS_ids = [f'TS_{i}' for i in range(n_timeSeries_perRegion)] 
+
+        ## time series 
+        data = np.zeros((n_timeSteps, n_regions, n_timeSeries_perRegion))
+
+        represented_timeSeries = xr.DataArray(data, [(time, time_steps),
+                                                    ('region_ids', region_ids),
+                                                    ('TS_ids', TS_ids)])
+
+        data = np.zeros((n_regions, n_timeSeries_perRegion))
+
+        #capacities
+        represented_capacities = xr.DataArray(data, [('region_ids', region_ids),
+                                                    ('TS_ids', TS_ids)])
+
+        #STEP 3. Clustering in every region...
         for region in region_ids:
-            results.append(_cluster_RE_technology(region))
-    
-    represented_RE_ds =  xr.merge(results)
-    
-    return represented_RE_ds 
+            #Preprocess regional data    
+            regional_capfac_da, regional_capacity_da, regional_power_da = _preprocess_regional_xr_ds(region)
+            
+            #Clustering
+            agg_cluster = AgglomerativeClustering(n_clusters=n_timeSeries_perRegion, 
+                                                affinity="euclidean",  
+                                                linkage=linkage)
+            agglomerative_model = agg_cluster.fit(regional_capfac_da)
+
+            #Aggregation
+            for i in range(np.unique(agglomerative_model.labels_).shape[0]):
+                ## Aggregate capacities 
+                cluster_capacity = regional_capacity_da[agglomerative_model.labels_ == i]
+                cluster_capacity_total = cluster_capacity.sum(dim = 'x_y').values
+
+                represented_capacities.loc[region, TS_ids[i]] = cluster_capacity_total
+
+                #aggregate capacity factor 
+                cluster_power = regional_power_da[agglomerative_model.labels_ == i]
+                cluster_power_total = cluster_power.sum(dim = 'x_y').values
+                cluster_capfac_total = cluster_power_total/cluster_capacity_total
+
+                represented_timeSeries.loc[:,region, TS_ids[i]] = cluster_capfac_total
+            
+        #STEP 4. Create resulting dataset 
+        regional_represented_RE_ds = xr.Dataset({capacity_var_name: represented_capacities,
+                                                capfac_var_name: represented_timeSeries})  
+
+        return regional_represented_RE_ds 
+
