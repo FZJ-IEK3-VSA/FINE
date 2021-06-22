1fc2e1882f5687ccbcf1dc724dcd97db164178ab
s.patil <s.patil@fz-juelich.de>
2021-06-22 15:27:00 +0200

    generateIterationDicts in utilsIO, code shortened for convertEsmInstanceToXarrayDataset and other minor changes

--- FINE/IOManagement/xarrayIO.py
+++ FINE/IOManagement/xarrayIO.py
@@ -2,10 +2,9 @@ import numpy as np
 import pandas as pd
 import xarray as xr
 
-from FINE import utils
-from FINE.IOManagement import dictIO
+from FINE.IOManagement import dictIO, utilsIO
 
-def saveNetcdfFile(xarray_dataset,  file_name='esM_instance.nc4'):
+def saveNetcdfFile(xarray_dataset, file_name='esM_instance.nc4'):
     """Saves the given xarray dataset as a netcdf file. 
     
     :param xarray_dataset: The dataset holding all data required to set up an esM instance. 
@@ -42,65 +41,6 @@ def saveNetcdfFile(xarray_dataset,  file_name='esM_instance.nc4'):
 
 
 
-def generateIterationDicts(component_dict):
-    """Creates iteration dictionaries that contain descriptions of all 
-    dataframes, series, and constants present in component_dict.
-    
-    :param component_dict: dictionary containing information about the esM instance's components
-    :type component_dict: dict
- 
-    :return: df_iteration_dict, series_iteration_dict, constants_iteration_dict
-    """
-    
-    df_iteration_dict, series_iteration_dict, constants_iteration_dict = {}, {}, {}
-
-    # Loop through every class-component-variable combination 
-    for classname in component_dict:
-
-        for component in component_dict[classname]:            
-
-            for variable_description, data in component_dict[classname][component].items():
-                description_tuple = (classname, component)
-                
-                #private function to check if the current variable is a dict, df, series or constant. 
-                # If its a dict (in the case of commodityConversionFactors), this is unpacked and the 
-                # the function is run on each value in dict 
-                def _append_to_iteration_dicts(_variable_description, _data):
-
-                    if isinstance(_data, dict):
-                        for key, value in _data.items():
-                            nested_variable_description = f'{_variable_description}.{key}' #NOTE: a . is introduced in the variable here  
-
-                            _append_to_iteration_dicts(nested_variable_description, value)
-
-                    elif isinstance(_data, pd.DataFrame):
-                        if _variable_description not in df_iteration_dict.keys():
-                            df_iteration_dict[_variable_description] = [description_tuple]
-                        else:
-                            df_iteration_dict[_variable_description].append(description_tuple)
-                    
-                    #NOTE: transmission components are series in component_dict 
-                    # (example index - cluster_0_cluster_2)
-                    
-                    elif isinstance(_data, pd.Series):       
-                        if _variable_description not in series_iteration_dict.keys():
-                            series_iteration_dict[_variable_description] = [description_tuple]
-                        else:
-                            series_iteration_dict[_variable_description].append(description_tuple)
-                            
-                    else:
-                        if _variable_description not in constants_iteration_dict.keys():
-                            constants_iteration_dict[_variable_description] = [description_tuple]
-                        else:
-                            constants_iteration_dict[_variable_description].append(description_tuple)
-
-                _append_to_iteration_dicts(variable_description, data)
-
-
-    return df_iteration_dict, series_iteration_dict, constants_iteration_dict
-
-
-
 def convertEsmInstanceToXarrayDataset(esM, save=False, file_name='esM_instance.nc4'):  
     """Takes esM instance and converts it into an xarray dataset. Optionally, the 
     dataset can be saved as a netcdf file.
@@ -116,155 +56,36 @@ def convertEsmInstanceToXarrayDataset(esM, save=False, file_name='esM_instance.n
         |br| * the default value is 'esM_instance.nc4'
     :type file_name: string
  
-    :return: ds - esM instance data in xarray dataset format 
+    :return: xr_ds - esM instance data in xarray dataset format 
     """
     
     #STEP 1. Get the esm and component dicts 
     esm_dict, component_dict = dictIO.exportToDict(esM)
 
-    locations = sorted(esm_dict['locations'])
-
     #STEP 2. Get the iteration dicts 
-    df_iteration_dict, series_iteration_dict, constants_iteration_dict = generateIterationDicts(component_dict)
+    df_iteration_dict, series_iteration_dict, constants_iteration_dict = \
+        utilsIO.generateIterationDicts(component_dict)
     
-    #STEP 3. Iterate through each iteration dicts and add the data to a xarray Dataset.
-    # data comes from component_dict
-    ds = xr.Dataset()
+    #STEP 3. Initiate xarray dataset 
+    xr_ds = xr.Dataset()
 
-    #STEP 3a. Add all regional time series (dimensions - space, time)
-    for variable_description, description_tuple_list in df_iteration_dict.items():
-        df_dict = {} 
+    #STEP 4. Add all df variables to xr_ds
+    xr_ds = utilsIO.addDFVariablesToXarray(xr_ds, component_dict, df_iteration_dict) 
 
-        for description_tuple in description_tuple_list:
-            classname, component = description_tuple
+    #STEP 5. Add all series variables to xr_ds
+    xr_ds = utilsIO.addSeriesVariablesToXarray(xr_ds, component_dict, series_iteration_dict, esm_dict['locations'])
 
-            df_description = f"{classname}, {component}"
-            
-            # If a . is present in variable name, then the data would be another level further in the component_dict 
-            if '.' in variable_description:      
-                nested_variable_description = variable_description.split('.')
-                data = component_dict[classname][component][nested_variable_description[0]][nested_variable_description[1]]
-            else:
-                data = component_dict[classname][component][variable_description]
+    #STEP 6. Add all constant value variables to xr_ds
+    xr_ds = utilsIO.addConstantsToXarray(xr_ds, component_dict, constants_iteration_dict) 
 
-            multi_index_dataframe = data.stack()
-            multi_index_dataframe.index.set_names("time", level=0, inplace=True)
-            multi_index_dataframe.index.set_names("space", level=1, inplace=True)
-            
-            df_dict[df_description] = multi_index_dataframe 
-                                        
-        df_variable = pd.concat(df_dict)
-        df_variable.index.set_names("component", level=0, inplace=True) 
+    #STEP 7. Add the data present in esm_dict as xarray attributes (these are dimensionless data). 
+    xr_ds.attrs = esm_dict
 
-        ds_component = xr.Dataset()
-        ds_component[f"ts_{variable_description}"] = df_variable.sort_index().to_xarray()
-
-        ds = xr.merge([ds, ds_component])
-
-    #STEP 3b. Add all 2d data (dimensions - space, space)
-    for variable_description, description_tuple_list in series_iteration_dict.items():
-        df_dict = {} 
-    
-        for description_tuple in description_tuple_list:
-            classname, component = description_tuple
-
-            df_description = f"{classname}, {component}"
-
-            if '.' in variable_description:
-                nested_variable_description = variable_description.split('.')
-                data = component_dict[classname][component][nested_variable_description[0]][nested_variable_description[1]]
-            else:
-                data = component_dict[classname][component][variable_description]
-
-            
-            if classname in ['Transmission', 'LinearOptimalPowerFlow']:    #NOTE: only ['Transmission', 'LinearOptimalPowerFlow'] are 2d classes 
-
-                df = utils.transform1dSeriesto2dDataFrame(data, locations)
-                multi_index_dataframe = df.stack()
-                multi_index_dataframe.index.set_names(["space", "space_2"], inplace=True)
-
-                df_dict[df_description] = multi_index_dataframe
-
-        if len(df_dict) > 0:
-            df_variable = pd.concat(df_dict)
-            df_variable.index.set_names("component", level=0, inplace=True) 
-            ds_component = xr.Dataset()
-            ds_component[f"2d_{variable_description}"] = df_variable.sort_index().to_xarray()  #NOTE: prefix 2d and 1d are added in this function
-
-            ds = xr.merge([ds, ds_component])
-
-    #STEP 3c. Add all 1d data (dimension - space) or time series (dimension - space (1) and time)
-    for variable_description, description_tuple_list in series_iteration_dict.items():
-
-        df_space_dict = {} 
-        df_time_dict = {} 
-        for description_tuple in description_tuple_list:
-            classname, component = description_tuple
-    
-            df_description = f"{classname}, {component}"
-
-            if '.' in variable_description:
-                nested_variable_description = variable_description.split(".")
-                data = component_dict[classname][component][nested_variable_description[0]][nested_variable_description[1]]
-            else:
-                data = component_dict[classname][component][variable_description]
-
-            if classname not in ['Transmission', 'LinearOptimalPowerFlow']:
-                if sorted(list(locations)) == sorted(list(data.index.values)):
-                    df_space_dict[df_description] = data.rename_axis("space")
-                else:
-                    df_time_dict[df_description] = data.rename_axis("time")
-                    df_time_dict[df_description] = pd.concat({locations[0]: df_time_dict[df_description]}, names=['space'])
-                    df_time_dict[df_description] = df_time_dict[df_description].reorder_levels(["time", "space"])
-            
-        if len(df_space_dict) > 0:
-            df_variable = pd.concat(df_space_dict)
-            df_variable.index.set_names("component", level=0, inplace=True) 
-            ds_component = xr.Dataset()
-            ds_component[f"1d_{variable_description}"] = df_variable.sort_index().to_xarray()
-            
-            ds = xr.merge([ds, ds_component])
-
-        if len(df_time_dict) > 0:
-            df_variable = pd.concat(df_time_dict)
-            df_variable.index.set_names("component", level=0, inplace=True) 
-            ds_component = xr.Dataset()
-            ds_component[f"ts_{variable_description}"] = df_variable.sort_index().to_xarray()
-            
-            ds = xr.merge([ds, ds_component])
-
-    #STEP 3e. Add all constants 
-    for variable_description, description_tuple_list in constants_iteration_dict.items():
-        
-        df_dict = {} 
-        for description_tuple in description_tuple_list:
-            classname, component = description_tuple
-            df_description = f"{classname}, {component}"
-
-            if '.' in variable_description:
-                nested_variable_description = variable_description.split(".")
-                data = component_dict[classname][component][nested_variable_description[0]][nested_variable_description[1]]
-            else:
-                data = component_dict[classname][component][variable_description]
-
-            df_dict[df_description] = data
-            
-        if len(df_dict) > 0:
-            df_variable = pd.Series(df_dict)      
-            df_variable.index.set_names("component", inplace=True)
-            
-            ds_component = xr.Dataset()
-            ds_component[f"0d_{variable_description}"] = xr.DataArray.from_series(df_variable)
-            
-            ds = xr.merge([ds, ds_component])
-            
-    #STEP 4. Add the data present in esm_dict as xarray attributes (these are dimensionless data). 
-    ds.attrs = esm_dict
-
+    #STEP 8. Save to netCDF file 
     if save:
-        saveNetcdfFile(ds, file_name)
+        saveNetcdfFile(xr_ds, file_name)
         
-    return ds
+    return xr_ds
 
 
 
@@ -362,7 +183,7 @@ def convertXarrayDatasetToEsmInstance(xarray_dataset):
 
     #STEP 4. Iterate through each component-variable pair, depending on the variable's prefix 
     # restructure the data and add it to component_dict 
-    component_dict = utils.PowerDict()
+    component_dict = utilsIO.PowerDict()
 
     for component in xarray_dataset.component.values:
         comp_xr = xarray_dataset.sel(component=component)
