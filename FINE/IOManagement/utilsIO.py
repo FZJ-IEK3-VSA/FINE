5d02c280d42d1274284fc682230c9e4ecbd1a68f
Kevin Knosala <k.knosala@fz-juelich.de>
2021-09-23 17:31:39 +0200

    Reduce number of seperator warnings

--- FINE/IOManagement/utilsIO.py
+++ FINE/IOManagement/utilsIO.py
@@ -28,14 +28,17 @@ def transform1dSeriesto2dDataFrame(series, locations, separator='_'):
 
     df = pd.DataFrame(values, columns=locations, index=locations)
 
+    for idx in series.index:
+        if idx.count("_") > 1:
+            warnings.warn(f'More than one {separator} found in series index. \
+            Therefore, {separator} is not used to split the index')
+            break
+
     for row in series.iteritems():
         
         try:
             id_1, id_2 = row[0].split(separator) 
         except:
-            warnings.warn(f'More than one {separator} found in series index. \
-            Therefore, {separator} is not used to split the index')
-
             row_center_id = math.ceil(len(row[0])/2)
             id_1, id_2 = row[0][:row_center_id-1], row[0][row_center_id:]
 
