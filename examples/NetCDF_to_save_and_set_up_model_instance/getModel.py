21079486a667a70e4f93b119ce5ae7b13038e3d5
Kevin Knosala <k.knosala@fz-juelich.de>
2021-09-23 17:25:37 +0200

    Update example

--- examples/NetCDF_to_save_and_set_up_model_instance/getModel.py
+++ examples/NetCDF_to_save_and_set_up_model_instance/getModel.py
@@ -2,109 +2,67 @@ import FINE as fn
 import pandas as pd
 import numpy as np
 
-from getData import getData
-
-data = getData()
-
 def getModel():
-    # 1. Create an energy system model instance
-    locations = {'cluster_0', 'cluster_1', 'cluster_2', 'cluster_3', 'cluster_4', 'cluster_5', 'cluster_6', 'cluster_7'}
-    commodityUnitDict = {'electricity': r'GW$_{el}$', 'methane': r'GW$_{CH_{4},LHV}$', 'biogas': r'GW$_{biogas,LHV}$',
-                        'CO2': r'Mio. t$_{CO_2}$/h', 'hydrogen': r'GW$_{H_{2},LHV}$'}
-    commodities = {'electricity', 'hydrogen', 'methane', 'biogas', 'CO2'}
+    numberOfTimeSteps = 4
+    hoursPerTimeStep = 2190
 
-    esM = fn.EnergySystemModel(locations=locations, commodities=commodities, numberOfTimeSteps=8760,
-                            commodityUnitsDict=commodityUnitDict,
-                            hoursPerTimeStep=1, costUnit='1e9 Euro', lengthUnit='km', verboseLogLevel=0)
+    # Create an energy system model instance 
+    esM = fn.EnergySystemModel(locations={'Electrolyzer_Location', 'IndustryLocation'}, 
+                                commodities={'electricity', 'hydrogen'}, 
+                                numberOfTimeSteps=numberOfTimeSteps,
+                                commodityUnitsDict={'electricity': r'kW$_{el}$', 'hydrogen': r'kW$_{H_{2},LHV}$'},
+                                hoursPerTimeStep=hoursPerTimeStep, costUnit='1 Euro', 
+                                lengthUnit='km', 
+                                verboseLogLevel=1,
+                                balanceLimit=None,
+                                lowerBound=False)
 
-    CO2_reductionTarget = 1
-
-
-    # 2. Add commodity sources to the energy system model
-    ### Wind onshore
-
-    esM.add(fn.Source(esM=esM, name='Wind (onshore)', commodity='electricity', hasCapacityVariable=True,
-                    operationRateMax=data['Wind (onshore), operationRateMax'],
-                    capacityMax=data['Wind (onshore), capacityMax'],
-                    investPerCapacity=1.1, opexPerCapacity=1.1*0.02, interestRate=0.08,
-                    economicLifetime=20))
-
-    ### PV
-
-    esM.add(fn.Source(esM=esM, name='PV', commodity='electricity', hasCapacityVariable=True,
-                    operationRateMax=data['PV, operationRateMax'], capacityMax=data['PV, capacityMax'],
-                    investPerCapacity=0.65, opexPerCapacity=0.65*0.02, interestRate=0.08,
-                    economicLifetime=25))
+    # time step length [h]
+    timeStepLength = numberOfTimeSteps * hoursPerTimeStep
 
 
-    # 3. Add conversion components to the energy system model
-
-    ### New combined cycly gas turbines for hydrogen
-    esM.add(fn.Conversion(esM=esM, name='New CCGT plants (hydrogen)', physicalUnit=r'GW$_{el}$',
-                        commodityConversionFactors={'electricity':1, 'hydrogen':-1/0.6},
-                        hasCapacityVariable=True,
-                        investPerCapacity=0.7, opexPerCapacity=0.021, interestRate=0.08,
-                        economicLifetime=33))
+    ### Buy electricity at the electricity market
+    costs = pd.DataFrame([np.array([ 0.05, 0., 0.1, 0.051,]),np.array([0., 0., 0., 0.,])],
+                            index = ['Electrolyzer_Location', 'IndustryLocation']).T
+    revenues = pd.DataFrame([np.array([ 0., 0.01, 0., 0.,]),np.array([0., 0., 0., 0.,])],
+                            index = ['Electrolyzer_Location', 'IndustryLocation']).T
+    maxpurchase = pd.DataFrame([np.array([1e6, 1e6, 1e6, 1e6,]),np.array([0., 0., 0., 0.,])],
+                            index = ['Electrolyzer_Location', 'IndustryLocation']).T * hoursPerTimeStep
+    esM.add(fn.Source(esM=esM, name='Electricity market', commodity='electricity', 
+                        hasCapacityVariable=False, operationRateMax = maxpurchase,
+                        commodityCostTimeSeries = costs,  
+                        commodityRevenueTimeSeries = revenues 
+                        )) # eur/kWh
 
     ### Electrolyzers
-    esM.add(fn.Conversion(esM=esM, name='Electroylzers', physicalUnit=r'GW$_{el}$',
-                        commodityConversionFactors={'electricity':-1, 'hydrogen':0.7},
-                        hasCapacityVariable=True,
-                        investPerCapacity=0.5, opexPerCapacity=0.5*0.025, interestRate=0.08,
-                        economicLifetime=10))
+    esM.add(fn.Conversion(esM=esM, name='Electrolyzers', physicalUnit=r'kW$_{el}$',
+                            commodityConversionFactors={'electricity':-1, 'hydrogen':0.7},
+                            hasCapacityVariable=True, 
+                            investPerCapacity=500, # euro/kW
+                            opexPerCapacity=500*0.025, 
+                            interestRate=0.08,
+                            economicLifetime=10))
 
-
-    # 4. Add commodity storages to the energy system model
-
-    ### Lithium ion batteries
-    esM.add(fn.Storage(esM=esM, name='Li-ion batteries', commodity='electricity',
-                    hasCapacityVariable=True, chargeEfficiency=0.95,
-                    cyclicLifetime=10000, dischargeEfficiency=0.95, selfDischarge=1-(1-0.03)**(1/(30*24)),
-                    chargeRate=1, dischargeRate=1, doPreciseTsaModeling=False,
-                    investPerCapacity=0.151, opexPerCapacity=0.002, interestRate=0.08,
-                    economicLifetime=22))
-
-    ### Hydrogen filled salt caverns
-    esM.add(fn.Storage(esM=esM, name='Salt caverns (hydrogen)', commodity='hydrogen',
-                    hasCapacityVariable=True, capacityVariableDomain='continuous',
-                    capacityPerPlantUnit=133,
-                    chargeRate=1/470.37, dischargeRate=1/470.37, sharedPotentialID='Existing salt caverns',
-                    stateOfChargeMin=0.33, stateOfChargeMax=1, capacityMax=data['Salt caverns (hydrogen), capacityMax'],
-                    investPerCapacity=0.00011, opexPerCapacity=0.00057, interestRate=0.08,
-                    economicLifetime=30))
-
-
-    # 5. Add commodity transmission components to the energy system model
-
-    ### AC cables
-    esM.add(fn.LinearOptimalPowerFlow(esM=esM, name='AC cables', commodity='electricity',
-                                    hasCapacityVariable=True, capacityFix=data['AC cables, capacityFix'],
-                                    reactances=data['AC cables, reactances']))
-
-    ### DC cables
-    esM.add(fn.Transmission(esM=esM, name='DC cables', commodity='electricity', losses=data['DC cables, losses'],
-                            distances=data['DC cables, distances'],
-                            hasCapacityVariable=True, capacityFix=data['DC cables, capacityFix']))
-
+    ### Hydrogen filled somewhere
+    esM.add(fn.Storage(esM=esM, name='Pressure tank', commodity='hydrogen',
+                        hasCapacityVariable=True, capacityVariableDomain='continuous',
+                        stateOfChargeMin=0.33, 
+                        investPerCapacity=0.5, # eur/kWh
+                        interestRate=0.08,
+                        economicLifetime=30))
 
     ### Hydrogen pipelines
-    esM.add(fn.Transmission(esM=esM, name='Pipelines (hydrogen)', commodity='hydrogen',
-                            distances=data['Pipelines, distances'],
-                            hasCapacityVariable=True, hasIsBuiltBinaryVariable=False, bigM=300,
-                            locationalEligibility=data['Pipelines, eligibility'],
-                            capacityMax=data['Pipelines, eligibility']*15, sharedPotentialID='pipelines',
-                            investPerCapacity=0.000177, investIfBuilt=0.00033,
-                            interestRate=0.08, economicLifetime=40))
+    esM.add(fn.Transmission(esM=esM, name='Pipelines', commodity='hydrogen',
+                            hasCapacityVariable=True,
+                            investPerCapacity=0.177, 
+                            interestRate=0.08, 
+                            economicLifetime=40))
 
-    # 6. Add commodity sinks to the energy system model
-
-    ### Electricity demand
-    esM.add(fn.Sink(esM=esM, name='Electricity demand', commodity='electricity',
-                    hasCapacityVariable=False, operationRateFix=data['Electricity demand, operationRateFix']))
-
-    ## 7.2. Hydrogen sinks
-    FCEV_penetration=0.5
-    esM.add(fn.Sink(esM=esM, name='Hydrogen demand', commodity='hydrogen', hasCapacityVariable=False,
-                    operationRateFix=data['Hydrogen demand, operationRateFix']*FCEV_penetration))
+    ### Industry site
+    demand = pd.DataFrame([np.array([0., 0., 0., 0.,]), np.array([6e3, 6e3, 6e3, 6e3,]),],
+                    index = ['Electrolyzer_Location', 'IndustryLocation']).T * hoursPerTimeStep
+    esM.add(fn.Sink(esM=esM, name='Industry site', commodity='hydrogen', hasCapacityVariable=False,
+                    operationRateFix = demand
+                    ))
 
     return esM
