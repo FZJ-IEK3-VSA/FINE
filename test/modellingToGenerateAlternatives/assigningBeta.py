47822f446c2fd810370295462c6bac8644211216
l.wijesinghe <l.wijesinghe@fz-juelich.de>
2025-05-08 08:41:08 +0200

    examples added

---
+++ test/modellingToGenerateAlternatives/assigningBeta.py
@@ -0,0 +1,104 @@
+import numpy as np
+import pandas as pd
+import fine as fn
+import random
+
+def calculateBeta(esM):
+
+    components = []
+    transmissionComponents = []
+
+    for item in esM.componentModelingDict.values():
+        for key,_item in item.componentsDict.items():
+            components.append(key)
+
+            if isinstance(_item, fn.transmission.Transmission):
+                transmissionComponents.append(key)
+
+    transmission_locations = []
+    for loc1 in esM.locations:
+        for loc2 in esM.locations:
+            if loc1 != loc2:
+                transmission_locations.append(loc1 + "_" + loc2)
+
+    esM.beta = {location: 
+            {iteration+1: 
+            {component: random.random() for component in components 
+             if component not in transmissionComponents               
+            }  
+            for iteration in range(esM.iterations)
+            } 
+            for location in esM.locations
+            }
+    
+    transmission_data = {location: 
+            {iteration+1: 
+            {component: random.random() for component in transmissionComponents
+            }  
+            for iteration in range(esM.iterations)
+            } 
+            for location in transmission_locations
+            }
+
+    esM.beta.update(transmission_data)
+
+esM = fn.EnergySystemModel(
+    locations={"Region1","Region2"},
+    commodities={"electricity","heat"},
+    numberOfTimeSteps=6,
+    commodityUnitsDict={"electricity": r"GW$_{el}$", "heat": r"GW$_{heat}$"},
+    hoursPerTimeStep=1,
+    costUnit="1e9 Euro",
+    lengthUnit="km",
+    verboseLogLevel=0
+)
+
+esM.add(
+    fn.Sink(
+        esM=esM,
+        name="Electricity demand",
+        commodity="electricity",
+        hasCapacityVariable=False,
+    )
+)
+
+esM.add(
+    fn.Source(
+        esM=esM,
+        name="wind",
+        commodity="electricity",
+        hasCapacityVariable=True,
+    )
+)
+
+esM.add(
+    fn.Conversion(
+        esM=esM,
+        name="Heat Pump",
+        physicalUnit=r"GW$_{el}$",
+        commodityConversionFactors={"electricity": -1/2.5, "heat": 1},
+        hasCapacityVariable=True,
+    )
+)
+
+esM.add(
+    fn.Storage(
+        esM=esM,
+        name="Li-ion batteries",
+        commodity="electricity",
+        hasCapacityVariable=True,
+    )
+)
+
+esM.add(
+    fn.Transmission(
+        esM=esM,
+        name="AC cables",
+        commodity="electricity",
+        hasCapacityVariable=True,
+    )
+)
+
+esM.iterations = 2
+calculateBeta(esM)
+print(esM.beta)
---
+++ test/modellingToGenerateAlternatives/checkMGAObjectiveFunction.py
@@ -0,0 +1,140 @@
+import numpy as np
+import pandas as pd
+import fine as fn
+import copy
+
+from pyomo import opt
+from pyomo.environ import *
+
+esM = fn.EnergySystemModel(
+    locations={"Region1","Region2"},
+    commodities={"electricity","heat"},
+    numberOfTimeSteps=6,
+    commodityUnitsDict={"electricity": r"GW$_{el}$", "heat": r"GW$_{heat}$"},
+    hoursPerTimeStep=1,
+    costUnit="1e9 Euro",
+    lengthUnit="km",
+    verboseLogLevel=0
+)
+
+demandProfile = [
+    0.6,
+    0.7,
+    0.9,
+    1,
+    0.9,
+    0.8,
+]
+
+demand = pd.DataFrame(
+    [[u * 10, u * 20] for u in demandProfile],
+    index=range(6),
+    columns=["Region1", "Region2"],
+)
+
+esM.add(
+    fn.Sink(
+        esM=esM,
+        name="Electricity demand",
+        commodity="electricity",
+        hasCapacityVariable=False,
+        operationRateFix=demand,
+    )
+)
+
+generationProfile = [
+    0.05,
+    0.15,
+    0.2,
+    0.0,
+    0.8,
+    0.7
+]
+operationRateFix = pd.DataFrame(
+    [[u*4, u*8] for u in generationProfile],
+    index=range(6),
+    columns=["Region1", "Region2"],
+)
+capacityMax = pd.Series([100, 120], index=["Region1", "Region2"])
+investPerCapacity, opexPerCapacity = 100, 10
+interestRate, economicLifetime = 0.08, 25
+esM.add(
+    fn.Source(
+        esM=esM,
+        name="wind",
+        commodity="electricity",
+        hasCapacityVariable=True,
+        operationRateFix=operationRateFix,
+        capacityFix=capacityMax,
+        investPerCapacity=investPerCapacity,
+        opexPerCapacity=opexPerCapacity,
+        interestRate=interestRate,
+        economicLifetime=economicLifetime,
+    )
+)
+
+esM.add(
+    fn.Conversion(
+        esM=esM,
+        name="Heat Pump",
+        physicalUnit=r"GW$_{el}$",
+        commodityConversionFactors={"electricity": -1/2.5, "heat": 1},
+        hasCapacityVariable=True,
+        investPerCapacity=0.76,
+        opexPerCapacity=0.00646,
+        interestRate=0.06,
+        economicLifetime=20,
+    )
+)
+
+esM.add(
+    fn.Storage(
+        esM=esM,
+        name="Li-ion batteries",
+        commodity="electricity",
+        hasCapacityVariable=True,
+        chargeEfficiency=0.95,
+        cyclicLifetime=10000,
+        dischargeEfficiency=0.95,
+        selfDischarge=1 - (1 - 0.03) ** (1 / (30 * 24)),
+        chargeRate=1,
+        dischargeRate=1,
+        doPreciseTsaModeling=False,
+        investPerCapacity=0.151,
+        opexPerCapacity=0.002,
+        interestRate=0.08,
+        economicLifetime=22,
+    )
+)
+
+acCables = pd.DataFrame(data =[[0,10],[10,0]], index=["Region1","Region2"], columns=["Region1","Region2"])
+print(acCables)
+
+esM.add(
+    fn.Transmission(
+        esM=esM,
+        name="AC cables",
+        commodity="electricity",
+        hasCapacityVariable=True,
+        capacityFix=acCables,
+    )
+)
+
+esM.optimize(
+    timeSeriesAggregation=False,
+    optimizationSpecs="OptimalityTol=1e-3 method=2 cuts=0 MIPGap=5e-3",
+)
+
+esM.objectiveValue = copy.deepcopy(esM.pyM.Obj())
+esM.slack = 0.1
+esM.iterations = 2
+fn.Mgaoptimize.calculateBeta(esM, random_seed=False)
+
+fn.Mgaoptimize.declareMGAOptimizationProblem(esM, iteration=1, sense="maximize")
+
+# Check manually the beta values in the corresponding objective function.
+# For example, if 'Region1': {1: {'Electricity demand'} in esM.beta is i, then i in 
+# i*op_srcSnk[Region1,Electricity demand,0,0,0] in the objective function must be the same (i is a float between 0 and 1).
+
+print(esM.beta)
+print(esM.pyM.Obj.expr)
