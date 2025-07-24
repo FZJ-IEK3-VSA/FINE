bae757b2c87870e7aeecd7754015164393d7b1d0
p.dunkel <p.dunkel@fz-juelich.de>
2025-07-24 14:15:17 +0200

    adapted test values because dimensioning of conversion component changes due to different factor time series. removed unnecessary tests

--- test/test_commodityConversionFactorsTimeSeries.py
+++ test/test_commodityConversionFactorsTimeSeries.py
@@ -106,45 +106,6 @@ def test_variable_conversion_simple_no_tsa():
     )
 
 
-def test_variable_conversion_simple_with_tsa():
-    """
-    This is to test if the conversion time series are temporally clustered properly.
-    Temporal clustering with 2 typical periods leads to two distinguished demand values
-    instead of 4 in the case without temporally clustered timeseries.
-    """
-
-    esM = create_simple_esm()
-
-    # Temporal clustering
-    esM.aggregateTemporally(
-        numberOfTypicalPeriods=2,
-        numberOfTimeStepsPerPeriod=1,
-        segmentation=False,
-        sortValues=True,
-        representationMethod=None,
-        rescaleClusterPeriods=True,
-    )
-    # Optimization
-    esM.optimize(timeSeriesAggregation=True, solver="glpk")
-
-    df = esM.componentModelingDict["SourceSinkModel"].operationVariablesOptimum
-
-    # Assert the optimal operation
-    # We are asserting up to a precision of one decimal to account for precision gaps
-    # of the solver.
-    np.testing.assert_almost_equal(
-        df.xs(("Electricity market", "ElectrolyzerLocation"))[0], 11575.7, decimal=1
-    )
-    np.testing.assert_almost_equal(
-        df.xs(("Electricity market", "ElectrolyzerLocation"))[1], 11575.7, decimal=1
-    )
-    np.testing.assert_almost_equal(
-        df.xs(("Electricity market", "ElectrolyzerLocation"))[2], 11575.7, decimal=1
-    )
-    np.testing.assert_almost_equal(
-        df.xs(("Electricity market", "ElectrolyzerLocation"))[3], 312857.1, decimal=1
-    )
-
 
 def test_basecase(minimal_test_esM):
     """
@@ -195,7 +156,7 @@ def test_variable_conversion_factor_no_tsa(minimal_test_esM):
         [np.array([-1, -1, -1, -1]), np.array([-1, -1, -1, -1])], index=locs
     ).T
     cfs["hydrogen"] = pd.DataFrame(
-        [np.array([-7.0, -0.7 , -0.7 , -0.07]), np.array([7, 0.7, 0.7, 0.07])], index=locs
+        [np.array([7.0, 0.7 , 0.7 , 0.07]), np.array([7, 0.7, 0.7, 0.07])], index=locs
     ).T
 
     # Add a new component with variable conversion rate to the EnergySystemModel.
@@ -238,87 +199,8 @@ def test_variable_conversion_factor_no_tsa(minimal_test_esM):
     # Assert the optimal operation
     # We are asserting up to a precision of one decimal to account for precision gaps
     # of the solver.
-    assertion_values_const = [0.0, 18771428.5, 0.0, 18771428.5]
-    assertion_values_var = [18771428.5, 18771428.5, 0.0, 0.0]
-    for t in range(0, 4):
-        np.testing.assert_almost_equal(
-            op_test_const[t], assertion_values_const[t], decimal=1
-        )
-        np.testing.assert_almost_equal(
-            op_test_var[t], assertion_values_var[t], decimal=1
-        )
-
-
-def test_variable_conversion_factor_with_tsa(minimal_test_esM):
-    """
-    Same as `test_variable_conversion_factor_no_tsa` but with time series aggregation
-    using 3 typical periods. Now the optimal solution is composed of only three different
-    periods.
-    """
-
-    # Get the minimal test system from conftest
-    esM = copy.deepcopy(minimal_test_esM)
-
-    # Create time-variable conversion rates for the two locations as pandas.DataFrame.
-    locs = ["ElectrolyzerLocation", "IndustryLocation"]
-    cfs = {}
-    cfs["electricity"] = pd.DataFrame(
-        [np.array([-1, -1, -1, -1]), np.array([-1, -1, -1, -1])], index=locs
-    ).T
-    cfs["hydrogen"] = pd.DataFrame(
-        [np.array([7, 0.7, 0.7, 0.07]), np.array([7, 0.7, 0.7, 0.07])], index=locs
-    ).T
-
-    # Add a new component with variable conversion rate to the EnergySystemModel.
-    esM.add(
-        fn.Conversion(
-            esM=esM,
-            name="Electrolyzers_VarConvFac",
-            physicalUnit=r"kW$_{el}$",
-            commodity="electricity",
-            commodityConversionFactors={
-                "electricity": cfs["electricity"],
-                "hydrogen": cfs["hydrogen"],
-            },
-            hasCapacityVariable=True,
-            investPerCapacity=1000,  # euro/kW
-            opexPerCapacity=500 * 0.025,
-            interestRate=0.08,
-            economicLifetime=10,
-        )
-    )
-
-    esM.aggregateTemporally(
-        numberOfTypicalPeriods=3,
-        numberOfTimeStepsPerPeriod=1,
-        segmentation=False,
-        sortValues=True,
-        representationMethod=None,
-        rescaleClusterPeriods=True,
-    )
-
-    esM.optimize(timeSeriesAggregation=True, solver="glpk")
-
-    # Get optimal electrolyzer operations
-    op_test_const = []
-    op_test_var = []
-    for t in range(0, 4):
-        op_test_const.append(
-            esM.componentModelingDict["ConversionModel"]
-            .operationVariablesOptimum.xs("Electrolyzers")
-            .loc["ElectrolyzerLocation", t]
-        )
-        op_test_var.append(
-            esM.componentModelingDict["ConversionModel"]
-            .operationVariablesOptimum.xs("Electrolyzers_VarConvFac")
-            .loc["ElectrolyzerLocation", t]
-        )
-
-    # Assert the optimal operation
-    # We are asserting up to a precision of one decimal to account for precision gaps
-    # of the solver.
-    assertion_values_const = [0.0, 9385714.2, 0.0, 9385714.2]
-    assertion_values_var = [18771428.5, 18771428.5, 18771428.5, 0.0]
+    assertion_values_const = [0.0, 14600000.0, 0.0, 14600000.0]
+    assertion_values_var = [4171428.6, 4171428.6, 0.0, 0.0]
     for t in range(0, 4):
         np.testing.assert_almost_equal(
             op_test_const[t], assertion_values_const[t], decimal=1
