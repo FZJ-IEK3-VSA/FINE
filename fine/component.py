e1069b948dbc72f81091d80c3d223c675ca1169d
j.behrens <j.behrens@fz-juelich.de>
2026-01-06 18:49:32 +0100

    ruff formatting

--- fine/component.py
+++ fine/component.py
@@ -2615,11 +2615,11 @@ class ComponentModel(metaclass=ABCMeta):
         Defines two constraints linking a continuous operation variable
         to its corresponding binary variable using the Big-M formulation.
         Handles both standard and commissioning year-dependent cases.
-        
+
         The binaryOperation1 constraint is used to force the binary variable
         to one if the continuous variable is greater than zero.
-        
-        The binaryOperation2 constraint ensures that the continuous 
+
+        The binaryOperation2 constraint ensures that the continuous
         variable is greater than zero whenever the binary variable is one.
         This is used for the upTimeMin and downTimeMin feature.
         """
