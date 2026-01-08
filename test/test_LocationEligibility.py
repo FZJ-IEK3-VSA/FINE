7a655378be05711c52db4841e9d1d6437cf5f1d1
l.wijesinghe <l.wijesinghe@fz-juelich.de>
2026-01-08 10:58:05 +0100

    test file added for location eligibility

---
+++ test/test_LocationEligibility.py
@@ -0,0 +1,37 @@
+# function to be adjusted: utils.setLocationalEligibility()
+
+import fine as fn
+import pandas as pd
+
+esm = fn.EnergySystemModel(
+    locations={"DE", "AT", "CH"},
+    commodities={"energy"},
+    commodityUnitsDict={"energy": "joule"})
+
+operationRateMax = pd.DataFrame(index=range(8760))
+capacityMax = pd.Series()
+
+operationRateMax["DE_CH"] = 0
+operationRateMax["AT_CH"] = 0.5
+operationRateMax["AT_DE"] = 0.5
+operationRateMax["CH_DE"] = 0
+operationRateMax["CH_AT"] = 0.5
+operationRateMax["DE_AT"] = 0.5
+
+capacityMax["DE_CH"] = 0.5
+capacityMax["AT_CH"] = 0
+capacityMax["AT_DE"] = 0.5
+capacityMax["CH_DE"] = 0.5
+capacityMax["CH_AT"] = 0
+capacityMax["DE_AT"] = 0.5
+
+esm.add(
+    fn.Transmission(
+        esM=esm,
+        name="transmission",
+        commodity="energy",
+        hasCapacityVariable = True,
+        capacityFix=capacityMax,
+        # operationRateMax=operationRateMax
+        )
+)
