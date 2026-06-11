f2e8acea991e6d3719c81f90725002b224a2b2f4
TobiasMios <t.miosga@fz-juelich.de>
2026-06-11 15:29:49 +0200

    coding style

--- test/test_plotFunctions.py
+++ test/test_plotFunctions.py
@@ -3,9 +3,9 @@ from pathlib import Path
 import types
 
 import geopandas as gpd
-import matplotlib
+import matplotlib as mpl
 
-matplotlib.use("Agg")
+mpl.use("Agg")
 
 import matplotlib.pyplot as plt
 import pandas as pd
