6a5ca3d5dc0f915e03268d1d262f76277f129249
j.behrens <j.behrens@fz-juelich.de>
2026-08-20 15:42:41 +0200

    update gurobipy import

--- fine/utils.py
+++ fine/utils.py
@@ -4,7 +4,6 @@ import warnings
 
 import numpy as np
 import pandas as pd
-import gurobipy as gp
 
 import fine as fn
 from fine.enums import Dimension, VarType
@@ -3116,6 +3115,7 @@ class ImplementedSolvers:
         env = None
         model = None
         try:
+            import gurobipy as gp
             env = gp.Env(empty=True)
             env.setParam("OutputFlag", 0)
             env.start()
