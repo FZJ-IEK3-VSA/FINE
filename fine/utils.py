759a4f52998a2722ba7f00c4fdf230f6a1f39567
TobiasMios <t.miosga@fz-juelich.de>
2026-08-25 15:14:11 +0200

    501 extend test coverage (#722)
    * Added several tests for functions, which were missing before.
    
    * coding style
    
    * minor changes due to test failed.
    
    * ruff.
    
    * ruff again.
    
    * Used esm from conftest.py in tests
    
    * ruff
    
    * ruff
    
    * .
    
    * copy esm fixture
    
    * Add example in ConversionPartLoad docstring that shows how to use a callable for commodityConversionFactorsPartLoad
    
    * Minor changes in test_minSOCwithTSAprecise.py and test_utils.py
    
    * remove test_plot.py and test_operationHeatMap in favor of new test_plotFunctions.py
    
    * remove obsolete folder
    
    ---------
    
    Co-authored-by: j.behrens <j.behrens@fz-juelich.de>
    Co-authored-by: p.dunkel <p.dunkel@fz-juelich.de>

--- fine/utils.py
+++ fine/utils.py
@@ -745,7 +745,6 @@ def checkCapacityDevelopmentWithStock(
                             + "commissioning and the technical lifetime) and "
                             + "capacityFix"
                         )
-
     if capacityFix is not None:
         if all(x is None for x in capacityFix.values()):
             return
